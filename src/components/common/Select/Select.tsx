96fc2be5de7fb3c2e2051ff58b234f2545df4fa5
truongdb <engtruong1609@gmail.com>
2024-10-16 17:00:11 +0700

    update

--- src/components/common/Select/Select.tsx
+++ src/components/common/Select/Select.tsx
@@ -5,11 +5,14 @@ import clsx from 'clsx';
 import styles from './Select.module.scss';
 import {GrSearch} from 'react-icons/gr';
 import {IoIosArrowDown} from 'react-icons/io';
+import TippyHeadless from '@tippyjs/react/headless';
 
 export const ContextSelect = createContext<any>({});
 
 function Select(props: PropsSelector) {
 	const ref = useRef<any>(null);
+
+	const [width, setWidth] = useState<number>(0);
 	const [show, setShow] = useState<boolean>(false);
 	const [value, setValue] = useState<any>(null);
 	const [keyword, setKeyword] = useState<string>('');
@@ -29,14 +32,20 @@ function Select(props: PropsSelector) {
 	};
 
 	useEffect(() => {
-		const handleClick = (e: any) => {
-			if (ref.current && !ref.current.contains(e.target)) {
-				setShow(false);
-			}
-		};
-		document.addEventListener('click', handleClick);
-		return () => document.removeEventListener('click', handleClick);
-	}, [ref]);
+		if (ref.current) {
+			const resizeObserver = new ResizeObserver((entries) => {
+				if (entries[0].contentRect) {
+					setWidth(entries[0].contentRect.width);
+				}
+			});
+
+			resizeObserver.observe(ref.current);
+
+			return () => {
+				resizeObserver.disconnect();
+			};
+		}
+	}, []);
 
 	return (
 		<ContextSelect.Provider
@@ -49,52 +58,63 @@ function Select(props: PropsSelector) {
 		>
 			<div className={styles.container}>
 				{props?.label ? <label>{props.label}</label> : null}
-				<div className={clsx(styles.select)} ref={ref}>
-					<div
-						className={clsx(styles.value, {[styles.active]: show, [styles.readOnly]: props.readOnly})}
-						onClick={() => {
-							if (props.readOnly) {
-								return null;
-							} else {
-								setShow(!show);
-							}
-						}}
-					>
-						<p
-							className={clsx(styles.text, {
-								[styles.placeholder]: !props.value,
+				<TippyHeadless
+					maxWidth={'100%'}
+					interactive
+					onClickOutside={() => setShow(false)}
+					placement='bottom-start'
+					visible={show}
+					render={(attrs: any) => (
+						<div
+							style={{width: width}}
+							className={clsx(styles.containerOption, {
+								[styles.active]: show,
 							})}
 						>
-							{props.value ? value?.title : props.placeholder}
-						</p>
-						<span className={styles.icon}>
-							<IoIosArrowDown size={16} />
-						</span>
-					</div>
-					<div
-						className={clsx(styles.containerOption, {
-							[styles.active]: show,
-						})}
-					>
-						{props.isSearch ? (
-							<div className={clsx(styles.search_group)}>
-								<div className={styles.icon}>
-									<GrSearch color='#005994' size={20} />
+							{props.isSearch ? (
+								<div className={clsx(styles.search_group)}>
+									<div className={styles.icon}>
+										<GrSearch color='#005994' size={20} />
+									</div>
+									<input
+										type='text'
+										placeholder='Tìm kiếm...'
+										onChange={(e) => setKeyword(e.target.value)}
+										value={keyword}
+										autoFocus={show}
+									/>
 								</div>
-								<input
-									type='text'
-									placeholder='Tìm kiếm...'
-									onChange={(e) => setKeyword(e.target.value)}
-									value={keyword}
-									autoFocus={show}
-								/>
+							) : null}
+							<div className={clsx(styles.main)} onClick={() => setShow(false)}>
+								{props.children}
 							</div>
-						) : null}
-						<div className={clsx(styles.main)} onClick={() => setShow(false)}>
-							{props.children}
+						</div>
+					)}
+				>
+					<div className={clsx(styles.select)} ref={ref}>
+						<div
+							className={clsx(styles.value, {[styles.active]: show, [styles.readOnly]: props.readOnly})}
+							onClick={() => {
+								if (props.readOnly) {
+									return null;
+								} else {
+									setShow(!show);
+								}
+							}}
+						>
+							<p
+								className={clsx(styles.text, {
+									[styles.placeholder]: !props.value,
+								})}
+							>
+								{props.value ? value?.title : props.placeholder}
+							</p>
+							<span className={styles.icon}>
+								<IoIosArrowDown size={16} />
+							</span>
 						</div>
 					</div>
-				</div>
+				</TippyHeadless>
 			</div>
 		</ContextSelect.Provider>
 	);
