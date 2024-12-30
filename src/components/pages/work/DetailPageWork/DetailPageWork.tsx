61ef8ae6aca270fd8cf2a3d4b6330d8d144ca320
TuanVT <vutrongtuan@gmail.com>
2024-12-30 14:54:23 +0700

    ff

--- src/components/pages/work/DetailPageWork/DetailPageWork.tsx
+++ src/components/pages/work/DetailPageWork/DetailPageWork.tsx
@@ -198,6 +198,7 @@ function DetailPageWork({}: PropsDetailPageWork) {
 										title: 'Tên nhà thầu',
 										render: (data: IContractByActivity) => <>{data?.contractor?.name}</>,
 									},
+									
 									{
 										title: 'Giá trị BLTHHĐ (VND) ',
 										render: (data: IContractByActivity) => <>{convertCoin(data?.contractExecution?.amount)}</>,
@@ -223,6 +224,10 @@ function DetailPageWork({}: PropsDetailPageWork) {
 											) : (
 												'---'
 											),
+									},
+									{
+										title: 'Người tạo hợp đồng',
+										render: (data: IContractByActivity) => <>{data?.user?.fullname}</>,
 									},
 									{
 										title: 'Trạng thái',
