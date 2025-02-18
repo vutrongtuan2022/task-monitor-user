9e7b31288068695268dd856cbcc98f219225200a
truongdb <engtruong1609@gmail.com>
2025-02-18 14:54:26 +0700

    fix contract

--- src/services/index.ts
+++ src/services/index.ts
@@ -11,7 +11,7 @@ const axiosClient = axios.create({
 		'content-type': 'application/json',
 	},
 	baseURL: process.env.NODE_ENV == 'development' ? process.env.NEXT_PUBLIC_API_URL_DEV : process.env.NEXT_PUBLIC_API_URL_PRODUCTION,
-	timeout: 15000,
+	timeout: 5 * 60 * 1000,
 	timeoutErrorMessage: 'Timeout error request',
 });
 
